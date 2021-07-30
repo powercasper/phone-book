1fa20d0c1a049e18496742726a81c2829a0d45da
serhii diachenko <sdiachenko@indeed.com>
2021-07-30 09:12:27 -0500

    main: updated

--- src/main/java/Contacts/ContactsList.java
+++ src/main/java/Contacts/ContactsList.java
@@ -3,7 +3,15 @@ package Contacts;
 import java.util.List;
 
 public class ContactsList extends InMemoryContactsService {
+    private final List<Contact> contactsList;
     public ContactsList(List<Contact> contacts) {
         super(contacts);
+        this.contactsList = contacts;
     }
+    public List<Contact> getAll() { return contactsList; }
+    public void add(Contact contact) { contactsList.add(contact); }
+    public void remove(int index) { contactsList.remove(index); }
+    public Contact get(int index) { return contactsList.get(index); }
+    public void setContact(int index, Contact contact) { }
+    public int size() { return contactsList.size(); }
 }
